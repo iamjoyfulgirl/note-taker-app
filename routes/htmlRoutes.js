b11372358861f1727aec7e87472be80ca7b8035c
iamjoyfulgirl <sherri@nymeria.lan>
2022-08-28 18:14:36 -0700

    Updates paths in htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,18 @@
+
 // dependencies
 const path = require('path');
+const fs = require('fs');
+
 
 // routing
 module.exports = (app) => {
 
     // creating routes
+    // GET / takes user to homepage
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
     // GET /notes should return the notes.html file.
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
