94091be22e54ac774c9fedd9b2d0e6b2f0d04683
iamjoyfulgirl <sherri@nymeria.lan>
2022-08-28 18:15:28 -0700

    Fixes route paths

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 
 // asks express to create a route for every file in the 'public' folder and give it a '/' route
-app.use(express.static('public'));
+app.use(express.static('./public'));
 // sets up express app to handle data parser, middleware created req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,3 +24,4 @@ require('./routes/htmlRoutes')(app);
 app.listen(PORT, () => {
     console.log(`Server available at localhost${PORT}`);
 });
+
